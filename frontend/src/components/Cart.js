e706d5bf53e03ff33f88881618d330ea4a8e1deb
Rogerslug <rogelio.020496@gmail.com>
2023-06-19 17:49:01 -0600

    ajustes

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,50 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Card, CardContent, Typography, Button } from '@mui/material';
+import { List, ListItem, ListItemText, Button } from '@mui/material';
 
 function Cart() {
-  const [cart, setCart] = useState(null);
-  const userId = 'userId'; // Reemplaza esto con el ID de usuario real
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/cart/${userId}`)
-      .then(response => response.json())
-      .then(data => setCart(data))
-      .catch(err => console.error(err));
-  }, [userId]);
+    // Aquí deberías hacer una solicitud GET para obtener el carrito del usuario
+    // Por ahora, solo vamos a usar datos de prueba
+    setCart([
+      { id: 1, name: 'Producto 1', price: 100 },
+      { id: 2, name: 'Producto 2', price: 200 },
+    ]);
+  }, []);
 
-  const handleRemove = (productId) => {
-    fetch(`http://localhost:5000/cart/${userId}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ productId }),
-    })
-      .then(response => response.json())
-      .then(data => setCart(data))
-      .catch(err => console.error(err));
+  const handleRemoveFromCart = (productId) => {
+    // Aquí deberías hacer una solicitud DELETE para eliminar el producto del carrito
+    // Por ahora, solo vamos a eliminar el producto del estado
+    setCart(cart.filter(product => product.id !== productId));
   };
 
-  if (!cart) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <Container>
-      <Typography variant='h2'>Carrito</Typography>
-      {cart.products.map((product) => (
-        <Card key={product._id}>
-          <CardContent>
-            <Typography variant='h5'>{product.name}</Typography>
-            <Typography variant='body1'>{product.description}</Typography>
-            <Typography variant='body1'>{product.price}</Typography>
-            <Button variant="contained" color="secondary" onClick={() => handleRemove(product._id)}>
-              Eliminar del carrito
-            </Button>
-          </CardContent>
-        </Card>
+    <List>
+      {cart.map(product => (
+        <ListItem key={product.id}>
+          <ListItemText primary={product.name} secondary={`Precio: ${product.price}`} />
+          <Button variant="contained" color="secondary" onClick={() => handleRemoveFromCart(product.id)}>
+            Eliminar
+          </Button>
+        </ListItem>
       ))}
-    </Container>
+    </List>
   );
 }
 
